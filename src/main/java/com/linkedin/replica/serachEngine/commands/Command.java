11b739f1fd71a1cc17e77543f0c8543c66995f13
HatemMorgan <hatemmorgan17@gmail.com>
2018-02-22 13:40:26 +0200

    Done with adding Commands

--- src/main/java/com/linkedin/replica/serachEngine/commands/Command.java
+++ src/main/java/com/linkedin/replica/serachEngine/commands/Command.java
@@ -1,20 +1,26 @@
 package com.linkedin.replica.serachEngine.commands;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
+
+import com.linkedin.replica.serachEngine.databaseHandlers.DatabaseHandler;
 /**
  * Command is an abstract class responsible for handling specific request and it communicates between
  * external input and internal functionality implementation
  */
 public abstract class Command {
     protected HashMap<String, String> args;
-
-    public Command(HashMap<String, String> args) {
+    protected DatabaseHandler dbHandler;
+    
+    public Command(HashMap<String, String> args, DatabaseHandler dbHandler) {
         this.args = args;
+        this.dbHandler = dbHandler;
     }
 
     /**
      * Execute the command
      * @return The output (if any) of the command
+     * 	LinkedHashMap preserve order of insertion so it will preserve this order when parsing to JSON
      */
-    public abstract String execute();
+    public abstract LinkedHashMap<String, Object> execute();
 }
--- src/main/java/com/linkedin/replica/serachEngine/commands/impl/SearchPostsCommand.java
+++ src/main/java/com/linkedin/replica/serachEngine/commands/impl/SearchPostsCommand.java
@@ -1,23 +1,27 @@
 package com.linkedin.replica.serachEngine.commands.impl;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 
 import com.linkedin.replica.serachEngine.commands.Command;
+import com.linkedin.replica.serachEngine.databaseHandlers.DatabaseHandler;
 
 /**
  *  Implementation of command design patterns for search for posts functionality
  */
 public class SearchPostsCommand extends Command{
 
-	public SearchPostsCommand(HashMap<String, String> args) {
-		super(args);
-		// TODO Auto-generated constructor stub
+	public SearchPostsCommand(HashMap<String, String> args, DatabaseHandler dbHandler) {
+		super(args, dbHandler);
 	}
 
 	@Override
-	public String execute() {
-		// TODO Auto-generated method stub
-		return null;
+	public LinkedHashMap<String, Object> execute() {
+		// create a LinkedHashMap to hold results 
+		LinkedHashMap<String,Object> results = new LinkedHashMap<String, Object>();
+		// call dbHandler to get results from db and add returned results to linkedHashMap
+		results.put("results", dbHandler.searchPosts(args.get("searchKey")));
+		return results;
 	}
 
 }
